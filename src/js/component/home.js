55f0f949d6b10911b7640914f4a9b511f337a5bc
Navarroseb <sebastnavarro@gmail.com>
2022-07-05 16:30:12 +0000

    Debuggin

---
+++ src/js/component/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+//create your first component
+export const Home = () => {
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">Hello World!</h1>
+			
+		</div>
+	);
+};
+
+
---
+++ src/js/layout.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
+import { Home } from "./component/home";
+import { Navbar } from "./component/navbar";
+/* import { Footer } from "./component/footer";
+import { Characters } from "./views/Characters";
+import { Charactersdetails } from "./views/Charactersdetails";
+import { Planets } from "./views/Planets";
+import { Planetsdetails} from "./views/Planetsdetails"; */
+
+
+
+
+const Layout = () => {
+	
+
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+					<Routes>
+					<Route path="/" element={<Home />} />
+						{/* <Route exact path="/Characters">
+							<Characters />
+						</Route>
+						<Route exact path="/Charactersdetails/:id">
+							<Charactersdetails />
+						</Route>
+						 <Route exact path="/Planets">
+							<Planets />
+						</Route>
+						<Route exact path="/Planetsdetails/:id">
+							<Planetsdetails />
+						</Route> */}
+					</Routes>
+					{/* <Footer /> */}
+			</BrowserRouter>
+		</>
+	);
+};
+
+export default Layout;
